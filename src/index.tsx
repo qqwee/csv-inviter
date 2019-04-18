51241213ae8e6bc23c1d7beb794a793e428175b8
Felix Gast <felix@subbr.com>
2019-04-18 05:19:52 +0200

    Removed thunk middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './session/session.reducers';
 import './index.css';
 
 const store = createStore(
   reducer,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
